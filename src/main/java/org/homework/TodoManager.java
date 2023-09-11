f4a9bac57513674a076aa7026ab8e05e30c5c66c
j2sunnn <j2sunnn@gmail.com>
2023-09-11 12:30:31 +0900

    step1 자동정렬, 예외처리 메소드, remove 메소드 수정하였습니다.

--- src/main/java/org/homework/TodoManager.java
+++ src/main/java/org/homework/TodoManager.java
@@ -6,16 +6,16 @@ public class TodoManager {
     private int id = 1;
     private final HashMap<Integer, String> todolist = new HashMap<>();
 
-    public int insert(String content) { //할 일의 내용을 입력 받아 Map에 저장
+    public int insert(String content) {
         todolist.put(id, content);
         return id++;
     }
 
-    public boolean delete(int id) {     //삭제할 할 일의 ID를 입력 받아 Map에서 해당 할 일을 삭제
+    public boolean delete(int id) {
         return todolist.remove(id) != null;
     }
 
-    public String select(int id) {      // 조회할 할 일의 ID를 입력 받아 Map에서 해당 할 일의 정보를 출력
+    public String select(int id) {
         return todolist.getOrDefault(id, "해당 ID의 할 일이 없습니다.");
     }
 
